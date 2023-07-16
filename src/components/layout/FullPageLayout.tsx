ea18210c68836028d24ae449533a1331683d7f12
96glory <click_syk@naver.com>
2023-07-16 14:00:10 +0900

    :sparkles: 컴포넌트 Space 추가

--- src/components/layout/FullPageLayout.tsx
+++ src/components/layout/FullPageLayout.tsx
@@ -5,13 +5,20 @@ import { Box, useMediaQuery, useTheme } from '@mui/material';
 import SoobookIntroduction from '@components/layout/SoobookIntroduction';
 import NavigationBar from '@components/navigation/NavigationBar';
 
+import Space from './Space';
+
 const FullPageLayout: FC<PropsWithChildren> = ({ children }) => {
   const theme = useTheme();
   const isPc = useMediaQuery(theme.breakpoints.up('md'));
 
   return (
-    <Box display="flex" justifyContent="space-around">
-      {isPc && <SoobookIntroduction />}
+    <Box display="flex" justifyContent="center">
+      {isPc && (
+        <>
+          <SoobookIntroduction />
+          <Space x={100} />
+        </>
+      )}
       <Box width={theme.breakpoints.values.sm} boxShadow="rgba(100, 100, 111, 0.2) 0px 7px 29px 0px">
         <NavigationBar />
         <>{children}</>
