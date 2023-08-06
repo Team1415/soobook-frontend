f86b423810133350f802791fb4dc544f9f4b32ca
96glory <click_syk@naver.com>
2023-08-06 23:19:15 +0900

    :sparkles: 검색창 정렬 조건 드롭박스 추가

--- src/components/select/SortSelect.style.tsx
+++ src/components/select/SortSelect.style.tsx
@@ -1,8 +1,39 @@
-import { styled, Typography } from '@mui/material';
+import { Box, styled } from '@mui/material';
 
-export const StyledTypography = styled(Typography)`
+export const SortSelectBox = styled(Box)`
+  display: flex;
+  align-items: center;
+`;
+
+export const StyledSelect = styled('select')`
+  width: 100%;
+  height: 17px;
+
+  display: flex;
+  align-items: center;
+
+  border: none;
+
+  padding-right: 12px;
+
   color: #203c8e;
   font-size: 11px;
   font-weight: 500;
   letter-spacing: -0.28px;
+  text-align: right;
+
+  background-image: url('/assets/down-arrow-icon.svg');
+  background-repeat: no-repeat;
+  background-position: 100% center;
+
+  cursor: pointer;
+
+  -o-appearance: none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+
+  ::-ms-expand {
+    display: none;
+  }
 `;
