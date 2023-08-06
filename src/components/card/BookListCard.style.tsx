ccadb128a5524e46452aac2dbd5d45ed0002ef50
jiho <tlswlgh120@naver.com>
2023-08-06 17:22:29 +0900

    :sparkles: Create BookListCard.tsx

--- src/components/card/BookListCard.style.tsx
+++ src/components/card/BookListCard.style.tsx
@@ -1,11 +1,52 @@
-import { Stack, styled } from '@mui/material';
+import { Stack, StackProps, styled, Typography } from '@mui/material';
 
-export const BookListCardStack = styled(Stack)`
-  width: 100%;
+const BookListCardStack = (props: StackProps) => (
+  <Stack direction="row" spacing="8px" {...props}>
+    {props.children}
+  </Stack>
+);
+
+export const StyledBookListCardStack = styled(BookListCardStack)`
+  width: calc(100% - 40px);
   background-color: #ffffff;
   height: 95px;
+  border-radius: 4px;
+  padding: 12px;
 `;
 
-export const ImageStack = styled(Stack)`
-  width: ;
+export const BookListImageStack = styled(Stack)`
+  height: 71px;
 `;
+
+export const BookListImage = styled('img')`
+  width: 55px;
+  height: 71px;
+  object-fit: cover;
+`;
+
+export const BookListBodyStack = styled(Stack)`
+  height: 71px;
+`;
+
+export const BookListTitleTypography = styled(Typography)`
+  height: 15px;
+  word-break: break-word;
+  font-size: 12px;
+  font-weight: 500;
+`;
+
+export const BookListPublisherTypography = styled(Typography)`
+  height: 14px;
+  color: #797979;
+  font-size: 11px;
+  font-weight: 400;
+  word-wrap: break-word;
+`;
+
+const BookListHashtagStack = (props: StackProps) => (
+  <Stack direction="row" spacing="4px">
+    {props.children}
+  </Stack>
+);
+
+export const StyledBookListHashtagStack = styled(BookListHashtagStack)``;
--- src/components/card/BookListCard.tsx
+++ src/components/card/BookListCard.tsx
@@ -1,11 +1,40 @@
+import HashtagBadge from '@components/badge/HashtagBadge';
+import {
+  BookListBodyStack,
+  BookListImage,
+  BookListImageStack,
+  BookListPublisherTypography,
+  BookListTitleTypography,
+  StyledBookListCardStack,
+  StyledBookListHashtagStack,
+} from '@components/card/BookListCard.style';
+import Space from '@components/layout/Space';
 import { BookListCardModel } from '@models/book';
 
-interface BookListCardProps {
-  book: BookListCardModel;
-}
+type BookListCardProps = BookListCardModel;
 
 const BookListCard = (props: BookListCardProps) => {
-  const { book } = props;
+  const { publisher, imageUrl, title, hashtags } = props;
 
-  return
+  return (
+    <StyledBookListCardStack>
+      <BookListImageStack>
+        <BookListImage src={imageUrl} alt={title} />
+      </BookListImageStack>
+      <BookListBodyStack>
+        <Space y={3} />
+        <BookListTitleTypography>{title}</BookListTitleTypography>
+        <Space y={4} />
+        <BookListPublisherTypography>{publisher}</BookListPublisherTypography>
+        <Space y={13} />
+        <StyledBookListHashtagStack>
+          {hashtags.map((hashtag) => (
+            <HashtagBadge name={hashtag} type="detail" key={`detail_hashtag_${hashtag}`} />
+          ))}
+        </StyledBookListHashtagStack>
+      </BookListBodyStack>
+    </StyledBookListCardStack>
+  );
 };
+
+export default BookListCard;
