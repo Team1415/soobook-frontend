17dec3b018868253440d19594b7f14461673f6bb
96glory <click_syk@naver.com>
2023-08-30 22:37:24 +0900

    :sparkles: 상세 페이지에 book 캐로셀 추가

--- src/pages/book/BookDetailPage.tsx
+++ src/pages/book/BookDetailPage.tsx
@@ -1,49 +1,67 @@
 import { useQuery } from '@tanstack/react-query';
-import { isEmpty } from 'lodash-es';
+import { isEmpty, map, slice } from 'lodash-es';
+import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
 
 import BookDetailTitle from '@components/bar/BookDetailTitle';
 import BookInformation from '@components/bar/BookInformation';
 import BookIntroduction from '@components/bar/BookIntroduction';
+import Title from '@components/bar/Title';
+import BookCarousel from '@components/carousel/BookCarousel';
 import GrayDivider from '@components/divider/GrayDivider';
 import BookDetailHeader from '@components/header/BookDetailHeader';
 import BookImageWithBlurredBackground from '@components/image/BookImageWithBlurredBackground';
-import { BookDetail } from '@models/book';
+import { BookSort } from '@constants/sort';
+import { Book, BookDetail } from '@models/book';
 import { BookService } from '@services/BookService';
 import { DateUtil } from '@utils/DateUtil';
 
 const BookDetailPage = () => {
+  const { t } = useTranslation();
   const { id } = useParams();
 
-  const { data, isLoading } = useQuery<BookDetail>(['books', id], () => BookService.getBook(id));
+  const { data: bookDetail, isLoading: isBookDetailLoading } = useQuery<BookDetail>(['books', id], () =>
+    BookService.getBook(id),
+  );
 
-  if (isLoading || isEmpty(data)) {
+  const hashtagIds = map(bookDetail?.hashtags, 'id');
+
+  const { data: relatedBooks, isLoading: isRelatedBooksLoading } = useQuery<Book[]>(
+    ['books', hashtagIds, BookSort.POPULAR],
+    () => BookService.getBooks(1, BookSort.POPULAR, hashtagIds), // TODO : category id로 변경
+    { enabled: !isBookDetailLoading, cacheTime: 1000 },
+  );
+
+  if (isBookDetailLoading || isEmpty(bookDetail)) {
     return <></>;
   } else {
     return (
       <>
         <BookDetailHeader />
 
-        <BookImageWithBlurredBackground imageUrl={data.thumbnailUrl} />
+        <BookImageWithBlurredBackground imageUrl={bookDetail.thumbnailUrl} />
 
-        <BookDetailTitle title={data.title} hashtags={data.hashtags} />
+        <BookDetailTitle title={bookDetail.title} hashtags={bookDetail.hashtags} />
         <GrayDivider />
 
         <BookInformation
-          authors={data.authors}
-          translators={data.translators}
-          publisher={data.publisher}
-          page={data.page}
-          publishDate={DateUtil.toString(data.publishDate)}
-          isbn={data.isbn10}
-          price={data.price}
+          authors={bookDetail.authors}
+          translators={bookDetail.translators}
+          publisher={bookDetail.publisher}
+          page={bookDetail.page}
+          publishDate={DateUtil.toString(bookDetail.publishDate)}
+          isbn={bookDetail.isbn10}
+          price={bookDetail.price}
         />
         <GrayDivider />
 
-        <BookIntroduction type='introduction' content={data.description} />
+        <BookIntroduction type='introduction' content={bookDetail.description} />
         <GrayDivider />
 
-        <BookIntroduction type='index' content={data.index} />
+        <BookIntroduction type='index' content={bookDetail.index} />
+
+        <Title leftLabel={t('detail-page.label.related')} />
+        <BookCarousel books={slice(relatedBooks, 0, 10) || []} />
       </>
     );
   }
